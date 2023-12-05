d2c3b0f30f46cbce0d061c2e83e173836f07d5e5
fadhlanzaky <fadhlanzaky01@gmail.com>
2023-12-05 14:06:17 +0700

    create tmp directory when starting.

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
-from flask import Flask
+import os
 
 from extensions.config_extension import register_configs
 from extensions.exception_extension import register_exception_handler
 from extensions.routes_extension import register_routes
+from flask import Flask
 
 def create_app():
     app = Flask(__name__)
@@ -11,5 +12,8 @@ def create_app():
     register_routes(app)
     register_exception_handler(app)
 
+    if not os.path.exists(os.environ.get('TMP_DIR')):
+        os.mkdir(os.environ.get('TMP_DIR'))
+        
     return app
 
